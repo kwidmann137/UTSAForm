bc84d5ec7430f78a04570ef6902e4c55a635f178
root <Kyle.Widmann@gmail.com>
2017-01-20 01:07:40 -0600

    remove jquery validation since not in use

--- FAQ/js/FAQContent.js
+++ FAQ/js/FAQContent.js
@@ -12,6 +12,14 @@ var FAQ = {
 		{
 			"question" : "Where do I send the completed and signed Performance Evaluation?",
 			"answer" : "Once the document has been printed and has all required signatures, send the original signed document to: UTSA - The Department of Human Resources: Attention Records Department"
+		},
+		{
+			"question" : "How do I rename my saved files?",
+			"answer" : "<strong>FIREFOX:</strong>  If you are using firefox, the browser will let you choose your download location and change the filename upon download by default.  When you hit save it will offer you the options of save or open.  Choose the save option, and save as you would save any other file on your computer.<br><br><strong>CHROME:</strong>  If you are using google chrome, the browser will automatically download the file with the formData.txt name in your downloads folder.  To change the filename, simply go to the file explorer for your computer and navigate to the downloads folder.  Once there find the desired formData.txt file, right click, and chose rename."
+		},
+		{
+			"question" : "How does my broswer handle downloads?",
+			"answer" : "<strong>FIREFOX:</strong>  If you are using firefox, the browser will let you choose your download location and change the filename upon download by default.  When you hit save it will offer you the options of save or open.  Choose the save option, and save as you would save any other file on your computer.<br><br><strong>CHROME:</strong>  If you are using google chrome, the browser will automatically download the file with the formData.txt name in your downloads folder.  You can find the file by using the file explorer for your operating system."
 		}
 	]
 }
