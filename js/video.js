fcb52744df98456d4e0b7a87e8432e7a82d1890b
root <Kyle.Widmann@gmail.com>
2017-01-25 01:00:22 -0600

    Added videos to save dialogue

--- js/video.js
+++ js/video.js
@@ -1,18 +1,21 @@
 function playVideo(ele){
 	if($(ele).text() === 'FireFox'){
 		console.log("FireFox");
-		var source = "Firefox";
+		var source1 = "./videos/Save-On-Firefox/Video/MP4/Save-On-Firefox.mp4";
+		var source2 = "./videos/Save-On-Firefox/Video/WebM/Save-On-Firefox.webm";
 	}else{
 		console.log("Chrome");
-		var source = "Chrome";
+		var source1 = "./videos/Save-On-Chrome/Video/MP4/Save-On-Chrome.mp4";
+		var source2 = "./videos/Save-On-Chrome/Video/WebM/Save-On-Chrome.webm";
 	}
 
 	$("#promptToSaveModal .modal-dialog").css('width', '900px');
-	$("#promptToSaveModal .video-container").css('width', '900px');
+	$("#promptToSaveModal .video-container").css('width', '868px');
 	$("#promptToSaveModal .video-container").css('height', '500px');
 
-	var video = '<video width="800" controls> <source src="%src%" type="video/mp4"> <source src="%src%" type="video/ogg"> Your browser does not support HTML5 video.</video>';
-	var formattedVideo = video.replace(/%src%/g, source);
+	var video = '<video width="800" controls> <source src="%src1%" type="video/mp4"> <source src="%src2%" type="video/ogg"> Your browser does not support HTML5 video.</video>';
+	var formattedVideo = video.replace(/%src1%/, source1);
+	var formattedVideo = formattedVideo.replace(/%src2%/, source2);
 	$("#promptToSaveModal .video-container").empty();
 	$("#promptToSaveModal .video-container").append(formattedVideo);
 }
