393203a5534ec48f3a9714ce1afbe709dd0c2970
root <Kyle.Widmann@gmail.com>
2017-02-02 08:40:31 -0600

    Added in individual video pages to avoid linking to youtube

---
+++ FAQ/OpenFile/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Open File Video</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+	<div class="video-container">
+		<iframe width="853" height="480" src="https://www.youtube.com/embed/AfUtCN3eyQc?rel=0" frameborder="0" allowfullscreen></iframe>
+	</div>
+</body>
+</html>
---
+++ FAQ/SaveOnChrome/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Save On Chrome Video</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+	<div class="video-container">
+		<iframe width="853" height="480" src="https://www.youtube.com/embed/fp3Oy-PPPl0?rel=0" frameborder="0" allowfullscreen></iframe>
+	</div>
+</body>
+</html>
