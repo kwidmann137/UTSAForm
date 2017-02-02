f0de38e875f0430f38f0205e02672fcd88eb6184
root <Kyle.Widmann@gmail.com>
2017-02-01 20:26:40 -0600

    Updated FAQ Content and tested printing

--- FAQ/js/FAQContent.js
+++ FAQ/js/FAQContent.js
@@ -6,20 +6,64 @@ var FAQ = {
 	// },
 	"items" : [
 		{
-			"question" : "Can I cut and paste last year’s form data to the new form?",
-			"answer" : "Yes"
+			"question" : "Q1.  Do I have to use this form?",
+			"answer" : "A. No. For this review period, 2016-2017, you may continue to use the evaluation form located at http://www.utsa.edu/hr/forms.html#7"
 		},
 		{
-			"question" : "Where do I send the completed and signed Performance Evaluation?",
-			"answer" : "Once the document has been printed and has all required signatures, send the original signed document to: UTSA - The Department of Human Resources: Attention Records Department"
+			"question" : "Q2.  What’s the difference between this evaluation form and the ones I’ve filled out previous years?",
+			"answer" : "A.  There is no content difference.  One form is a word document and the other is on the web.  The process remains the same; to complete the evaluation form you will use the information from previous years’ performance evaluation reviews, emails, notes, and any additional materials for each of your direct reports."
 		},
 		{
-			"question" : "How do I rename my saved files?",
-			"answer" : "<strong>FIREFOX:</strong>  If you are using firefox, the browser will let you choose your download location and change the filename upon download by default.  When you hit save it will offer you the options of save or open.  Choose the save option, and save as you would save any other file on your computer.<br><br><strong>CHROME:</strong>  If you are using google chrome, the browser will automatically download the file with the formData.txt name in your downloads folder.  To change the filename, simply go to the file explorer for your computer and navigate to the downloads folder.  Once there find the desired formData.txt file, right click, and chose rename."
+			"question" : "Q3.  Does the form work on all computers?",
+			"answer" : "A.  Yes. However, Firefox and Chrome are the recommended browsers.  Internet Explorer is not supported for this form."
 		},
 		{
-			"question" : "How does my broswer handle downloads?",
-			"answer" : "<strong>FIREFOX:</strong>  If you are using firefox, the browser will let you choose your download location and change the filename upon download by default.  When you hit save it will offer you the options of save or open.  Choose the save option, and save as you would save any other file on your computer.<br><br><strong>CHROME:</strong>  If you are using google chrome, the browser will automatically download the file with the formData.txt name in your downloads folder.  You can find the file by using the file explorer for your operating system."
-		}
+			"question" : "Q4.  What do the buttons mean on the top right of the form?",
+			"answer" : "A.  The buttons have been designed as tools to help answer any questions that you may have as you navigate the form.  They are:  <ul><li><strong>FAQ:</strong> Contains answers to common questions. We highly recommend that you read the FAQ prior to completing your first evaluation form.</li><li><strong>Tutorial:</strong> This is a step by step guide of how to complete, Save, Open, and Print the form. We highly recommend you complete the tutorial prior to completing your first evaluation form.</li><li><strong>Open:</strong> allows you to open a file you previously saved from this form.</li><li><strong>Save:</strong> allows you to save the data you entered on the form so you can continue working at a later time.</li><li><strong>Print for Review:</strong> This generates a pdf document without the signature page so that you and your employee can review it prior to finalizing the document.</li><li><strong>Print Final:</strong> This generates a pdf document with a signature page. The document must be free of all errors in order to print.</li><li><strong>Clear Form:</strong> Clears all the current form data and filename so you can begin and new evaluation for a different employee</li></ul>"
+		},
+		{
+			"question" : "Q5.  Can I use previous performance evaluation forms and transfer the information onto the new form? ",
+			"answer" : "A.   Yes.  The form allows you to copy and paste data into each section.  You may want to have your staff’s previous evaluation forms open on your computer to be able to cut and paste all of the previous information, essential job functions, standards and add your comments, projects and development plans."
+		},
+		{
+			"question" : "Q6. Do I have to fill in the Attribute section?",
+			"answer" : "A.  Yes.  The attribute section also includes a definition of each attribute (if you would like to use and edit the definition) for your employee.  Clicking on the clipboard icon will auto populate with the description.  If you are completing the performance evaluation form for a supervisor, the following section requires that you answer ‘yes’ to the question ”Is this employee a supervisor?” at which time additional attributes are displayed to be completed."
+		},
+		{
+			"question" : "Q7. How do I <STRONG>SAVE</STRONG> the evaluation form for each of my employees? ",
+			"answer" : 'A. If you are saving for one or multiple employees it is highly recommended you rename these files in order to make it easier to keep track and store them for later use. <strong>(Click on the green SAVE button for a video tutorial)</strong><ul><li>Select SAVE button - this Saves the formData.txt to the Downloads folder</li><li>Using File Explorer, navigate to the saved file location in the Downloads folder. Use the refresh button to view the current file.</li><li>Rename the file as a NewFileName.txt. Each Save thereafter will create a new version of the file in the Downloads folder: NewFileName(1).txt</li></ul><br><a href="https://youtu.be/fp3Oy-PPPl0" target="_blank">View Save On Chrome Video</a><br><a href="https://youtu.be/v9GEKwewzV4" target="_blank">View Save On FireFox Video</a>'
+		},
+		{
+			"question" : "Q8.  How do I <STRONG>OPEN</STRONG> A saved document? ",
+			"answer" : 'A.  Saving this form will save the file on your local computer. You can access the file via the File Explorer in order to move, or rename the file. Renaming is highly suggested so you know which employee the form is for. <strong>(Click the blue OPEN button for video tutorial)</strong><ul><li>Select Open button and Navigate to the New File Location/Name</li><li>Open the latest version of the saved file.</li></ul><br><a href="https://youtu.be/AfUtCN3eyQc" target="_blank">View Open File Video</a>'
+		},
+		{
+			"question" : "Q9.  How do I print the document for reviewing with my employee?",
+			"answer" : "A.  Select the <strong>Print for Review</strong> button, this generates a pdf document without the signature page. The document must be free of all errors in order to print. "
+		},
+		{
+			"question" : "Q10.  How do I share the form with my employee so they can review? ",
+			"answer" : "A.  To share the form with an employee you must print the document. You must give it to your employee to hand write any changes they recommend."
+		},
+		{
+			"question" : "Q11.  How do I update the changes my employee suggests? ",
+			"answer" : "A.  If your employee makes changes, adds information, or deletes information, you must make these changes on your original file. Follow the instructions on the tutorial to <strong>Open a Saved File</strong>."
+		},
+		{
+			"question" : "Q12. If I print the final evaluation for my emloyee to sign and they wish to add comments in the Employee Comment section, how do I incorporate them into the saved version?",
+			"answer" : "A. Have the employee send an email with their comments and then you will be able to edit your version or they may write their comments on the printed evaluation. Both employee and supervisor must sign the evaluation form."
+		},
+		{
+			"question" : "Q13. The form process is now complete and ready for final print and signatures. What is the last step in the process?",
+			"answer" : "A. Select the red <strong>Print Final</strong> button. This will generate a PDF with a signature page.  The document must be free of all errors in order to print."
+		},				
+		{
+			"question" : "Q14. Where do I send the completed and signed Performance Evaluation?",
+			"answer" : "Send by campus mail to UTSA - Office of Human Resources, Attention HR Records or email to HR-Records@utsa.edu - Completed forms must have all required signatures."
+		},
+		{
+			"question" : "Q15. Who do I contact for support?",
+			"answer" : "A. Technical: Email: HR-Records@utsa.edu or phone: x7875 and for Non-Technical/Performance Review Process: Email:Compensation@utsa.edu or phone x5916, x4651."
+		}		
 	]
 }
